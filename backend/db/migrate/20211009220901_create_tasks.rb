b31f6d81a8c586b4e00ccc0f28bf66a8a3f48f99
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-10-09 15:13:40 -0700

    Setup initial project file structure

---
+++ backend/db/migrate/20211009220901_create_tasks.rb
@@ -0,0 +1,14 @@
+class CreateTasks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :tasks do |t|
+      t.string :name
+      t.string :description
+      t.integer :project_id
+      t.integer :priority
+      t.date :dueDate
+      t.boolean :complete
+
+      t.timestamps
+    end
+  end
+end
