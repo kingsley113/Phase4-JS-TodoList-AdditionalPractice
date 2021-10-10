95f1e3fb3883a725566659913d24ced21de942b1
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-10-09 23:57:16 -0700

    Add trigger to show modal window

--- frontend/index.js
+++ frontend/index.js
@@ -26,6 +26,15 @@ class Project {
   }
 
   // function for # of incomplete tasks
+	incompleteTasks() {
+		let incompleteTaskArr = [];
+		for (const task of this.tasks) {
+			if (task.complete === false) {
+				incompleteTaskArr.push(task)
+			} 
+		}
+		return incompleteTaskArr;
+	}
 }
 
 class User {
@@ -49,7 +58,12 @@ document.addEventListener("DOMContentLoaded", (event) => {
 })
 
 function initialize() {
-	// TODO: move this probably to a separate function and trigger if currentUser = null
+	if(currentUser = null) {
+		showUserModal();
+	}
+}
+
+function showUserModal() {
 	btn.addEventListener("click", () => {
 		userModal.style.display = "block";
 	})
@@ -58,18 +72,18 @@ function initialize() {
 		userModal.style.display = "none";
 		console.log("closeBtn clicked")
 	})
-
+	
 	window.addEventListener("click", (event) => {
 		if (event.target == userModal) {
 			userModal.style.display = "none";
 		};
 	})
+	// TODO: create new user
+	// TODO: promp for user to enter new name or select from existing users
 }
-
 /* 
 Things we need to do:
 submit new task
-create new user
 fetch list of tasks
 fetch list of projects
 display list of tasks
@@ -80,7 +94,6 @@ delete task
 set task priority
 
 fetch user set of data
-promp for user to enter new name or select from existing users
 
 shortcut buttons on each task line:
 	mark complete
