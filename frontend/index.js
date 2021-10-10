f3b0dbeb059685d95ddf802c5a43a5b3ab848b3d
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-10-10 11:19:49 -0700

    Building out select user form

--- frontend/index.js
+++ frontend/index.js
@@ -5,6 +5,9 @@ let activeProject = null;
 let userModal;
 let btn;
 let closeBtn;
+// User Form
+let userList;
+let username;
 
 // declare classes
 class Task {
@@ -27,15 +30,15 @@ class Project {
   }
 
   // function for # of incomplete tasks
-	incompleteTasks() {
-		let incompleteTaskArr = [];
-		for (const task of this.tasks) {
-			if (task.complete === false) {
-				incompleteTaskArr.push(task)
-			} 
-		}
-		return incompleteTaskArr;
-	}
+  incompleteTasks() {
+    let incompleteTaskArr = [];
+    for (const task of this.tasks) {
+      if (task.complete === false) {
+        incompleteTaskArr.push(task);
+      }
+    }
+    return incompleteTaskArr;
+  }
 }
 
 class User {
@@ -51,38 +54,82 @@ class User {
 
 // Identify page elements
 document.addEventListener("DOMContentLoaded", (event) => {
-	userModal = document.getElementById("user-modal");
-	btn = document.getElementById("open-modal")
-	closeBtn = document.getElementById("close-user-modal")
+  userModal = document.getElementById("user-modal");
+  btn = document.getElementById("open-modal");
+  closeBtn = document.getElementById("close-user-modal");
 
-	initialize();	
-})
+  // User Form elements
+  userList = document.getElementById("existing-users");
+  username = document.getElementById("username-input");
+
+  initialize();
+});
 
 function initialize() {
-	if(currentUser === null) {
-		showUserModal();
-	}
+  if (currentUser === null) {
+    showUserModal();
+  }
 }
 
 function showUserModal() {
-	// TODO: make this trigger without button, keep button for debug only
-	btn.addEventListener("click", () => {
-		userModal.style.display = "block";
-	})
-		
-	// TODO: User shouldnt be able to close window without entering name or selecting user.
-	closeBtn.addEventListener("click", () => {
-		userModal.style.display = "none";
-		console.log("closeBtn clicked")
-	})
-	
-	window.addEventListener("click", (event) => {
-		if (event.target == userModal) {
-			userModal.style.display = "none";
-		};
-	})
-	// TODO: create new user
-	// TODO: promp for user to enter new name or select from existing users
+  // TODO: make this trigger without button, keep button for debug only
+  btn.addEventListener("click", () => {
+    userModal.style.display = "block";
+  });
+
+  // TODO: User shouldnt be able to close window without entering name or selecting user.
+  closeBtn.addEventListener("click", () => {
+    userModal.style.display = "none";
+    console.log("closeBtn clicked");
+  });
+
+  window.addEventListener("click", (event) => {
+    if (event.target == userModal) {
+      userModal.style.display = "none";
+    }
+  });
+
+  // userModal.style.display = "block";
+  populateUserFormList();
+  const userForm = document.getElementById("user-form");
+  userForm.addEventListener("submit", (event) => {
+    handleUserFormSubmit(event);
+  });
+  // TODO: create new user
+  // TODO: prompt for user to enter new name or select from existing users
+}
+
+function populateUserFormList() {
+  // fetch existing users
+  const users = fetchUsers();
+
+  // create option elements
+
+  for (const user of users) {
+    const option = document.createElement("option");
+    option.id = user.id;
+    option.value = user.id;
+    option.innerText = user.username;
+
+    // append option elements
+    userList.appendChild(option);
+  }
+}
+
+function fetchUsers() {
+  // TODO: setup fetch function
+  // TODO: setup API route to return only username, id,
+}
+
+function handleUserFormSubmit(event) {
+  event.preventDefault();
+  if (username.innerText !== "") {
+    // TODO: submit new user to API
+    createNewUser(username.innerText);
+  } else {
+    // TODO: submit selected existing user to API
+    loadExistingUser(document.getElementById("existing-users").value);
+  }
 }
 /* 
 Things we need to do:
