915bd8bd21ce7976e8a45cda1a524c66c49f40c1
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-10-10 17:08:10 -0700

    Add Seeds to seed file

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Create Users
+jimmy = User.create(username: "Jimmy")
+david = User.create(username: "David")
+bob = User.create(username: "Bob")
+
+# Create Projects
+p1 = Project.create(name: "R2D2", user: bob)
+p2 = Project.create(name: "Speakers", user: david)
+p3 = Project.create(name: "Tv Show", user: jimmy)
+p4 = Project.create(name: "Lego Table", user: bob)
+p5 = Project.create(name: "Go Kart", user: david)
+p6 = Project.create(name: "Guitar", user: jimmy)
+
